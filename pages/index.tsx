d095c35d9a32d64545a5dca1807dc208c970cab5
matthew joseph <matthew.j.joe@gmail.com>
2022-07-10 17:08:22 -0400

    added nav page/component

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,36 +2,18 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import Nav from './nav'
 
-interface TemplateName {
-  name : string,
-  age  : number, 
-  calcFavColor : Function,
-}
-
-function formatUser(user : TemplateName){
-  return user.name + ' is ' + user.age + user.calcFavColor()
-}
-
-const UserOne : TemplateName = {
-  name : "Matt",
-  age  : 25,
-  calcFavColor : ()=>{
-    return "Green"
-  }
-}
-
-let numbers = [1, 9]
-const addNums = <h1> Your toal is: {numbers[0] + numbers[1]} </h1>
-
-
-
-// return variable 
 const Home: NextPage = () => {
   return (
+    <>
+    <Nav/>
+
     <div className={styles.container}>
-      {addNums}
+      <h1>Hello, world.</h1>
     </div>
+    </>
   )
 }
+
 export default Home
