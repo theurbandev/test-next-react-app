03182c00a21f44d7d125c5eb819fb5821c833572
matthew joseph <matthew.j.joe@gmail.com>
2022-07-12 23:56:47 -0400

    added event listener to side nav bttns

--- pages/sideNav.tsx
+++ pages/sideNav.tsx
@@ -1,14 +1,19 @@
 import type { NextPage } from 'next'
 import styles from '../styles/nav.module.css'
 
+
 const SideNav : NextPage = () => {
     return (
         <div className={styles.container} id={styles.sideNav}>
-            <h1 className={styles.menuButtons}>Add Task</h1>
-            <h1 className={styles.menuButtons}>Delete Task</h1>
-            <h1 className={styles.menuButtons}>Undo</h1>
+            <h1 className={styles.menuButtons} onClick={(e) => clicked("Add")}>Add Task</h1>
+            <h1 className={styles.menuButtons} onClick={(e) => clicked("Remove")}>Delete Task</h1>
+            <h1 className={styles.menuButtons} onClick={(e) => clicked("Undo")}>Undo</h1>
         </div>
     )
+}
+
+function clicked(title : string){
+    alert(title);
 }
 
 export default SideNav
