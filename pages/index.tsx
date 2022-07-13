3bf7fed8f987eb07c8a1bbbffdc34d372c8c9e57
matthew joseph <matthew.j.joe@gmail.com>
2022-07-12 23:27:08 -0400

    added grid

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,9 +8,15 @@ const Home: NextPage = () => {
   return (
     <>
     <h1 className={styles.center}>To-Do</h1>
-      <SideNav/>
-    <div className={styles.container}>
-      <h1>Hello, world.</h1>
+    <div className={styles.gridContainer}>
+        <div className='grid-item'>
+          <SideNav/>
+        </div>
+
+      <div className="grid-item">
+        <h1 className={styles.lightFont}>All Tasks Complete!</h1>
+      </div>
+
     </div>
     </>
   )
