5cfc379a7788afbfc1491a196f04117cc587be3b
matthew joseph <matthew.j.joe@gmail.com>
2022-07-13 00:12:41 -0400

    added card component

---
+++ pages/card.tsx
@@ -0,0 +1,12 @@
+import type { NextPage } from 'next'
+
+
+const Card : NextPage = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Card
--- pages/sideNav.tsx
+++ pages/sideNav.tsx
@@ -5,9 +5,9 @@ import styles from '../styles/nav.module.css'
 const SideNav : NextPage = () => {
     return (
         <div className={styles.container} id={styles.sideNav}>
-            <h1 className={styles.menuButtons} onClick={(e) => clicked("Add")}>Add Task</h1>
-            <h1 className={styles.menuButtons} onClick={(e) => clicked("Remove")}>Delete Task</h1>
-            <h1 className={styles.menuButtons} onClick={(e) => clicked("Undo")}>Undo</h1>
+            <h1 className={styles.menuButtons} onClick={()=> clicked("Add")}>Add Task</h1>
+            <h1 className={styles.menuButtons} onClick={()=> clicked("Remove")}>Delete Task</h1>
+            <h1 className={styles.menuButtons} onClick={()=> clicked("Undo")}>Undo</h1>
         </div>
     )
 }
