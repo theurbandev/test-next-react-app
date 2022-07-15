d351648931a39fbd7b3b645a47faf6603ea06ccd
matthew joseph <matthew.j.joe@gmail.com>
2022-07-14 23:57:30 -0400

    comment

--- pages/sideNav.tsx
+++ pages/sideNav.tsx
@@ -5,6 +5,7 @@ import styles from '../styles/nav.module.css'
 const SideNav : NextPage = () => {
     return (
         <div className={styles.container} id={styles.sideNav}>
+            {/* can prob find a way to use .map() here */}
             <h1 className={styles.menuButtons} onClick={()=> clicked("Add")}>Add Task</h1>
             <h1 className={styles.menuButtons} onClick={()=> clicked("Remove")}>Delete Task</h1>
             <h1 className={styles.menuButtons} onClick={()=> clicked("Undo")}>Undo</h1>
