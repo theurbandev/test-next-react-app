ac5a06328b2a1c74a7249f1851129846f50e7b9f
Matthew Joseph <matthew.j.joe@gmail.com>
2022-07-22 01:13:01 -0400

    messing around with props

--- pages/sideNav.tsx
+++ pages/sideNav.tsx
@@ -1,14 +1,20 @@
 import type { NextPage } from 'next'
 import styles from '../styles/nav.module.css'
 
-const SideNav : NextPage = () => {
+interface Props{
+    name : string,
+    age? : number
+};
+
+export const SideNav = (props: Props) => {
     return (
         <div className={styles.container} id={styles.sideNav}>
+            <h2>{props.name}</h2>
             <h1 className={styles.menuButtons}>Add Task</h1>
             <h1 className={styles.menuButtons}>Delete Task</h1>
             <h1 className={styles.menuButtons}>Undo</h1>
         </div>
     )
-}
+};
 
 export default SideNav
