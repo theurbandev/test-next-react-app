160ac784430894952b96d66acb4465ca5092c67f
Matthew Joseph <matthew.j.joe@gmail.com>
2022-07-27 00:06:13 -0400

    added reusable bttns to side nav

--- pages/buttons.tsx
+++ pages/buttons.tsx
@@ -1,15 +1,18 @@
 import type { NextPage } from 'next'
 import { text } from 'stream/consumers';
+import styles from '../styles/buttons.module.css'
 
-interface Props{
+interface ButtonProps{
+    fontSize : string,
     text : string,
     backgroundColor? : string,
-    textColor? : string
+    textColor? : string,
+    padding: string
 };
 
-export const Button = (props: Props) => {
+export const Button = (props: ButtonProps) => {
     return (
-        <button style={{backgroundColor : props.backgroundColor, color : props.textColor}}>{props.text}</button>
+        <button style={{backgroundColor : props.backgroundColor, color : props.textColor, fontSize : props.fontSize, padding : props.padding, width: "100%", margin : "5px 0px"}}>{props.text}</button>
     )
 };
 
--- pages/sideNav.tsx
+++ pages/sideNav.tsx
@@ -1,36 +1,26 @@
 import type { NextPage } from 'next'
 import styles from '../styles/nav.module.css'
-
-// internal imports
 import Button from './buttons'
 
-interface Props{
-    name : string,
-    age? : number
+interface NavProps{
+    name : string
 };
 
 function HandleBttnClick(event : string){
     alert({event} + " has happended");
 }
 
-const bttnArr = [
-    "Add Task", "Delete Task", "Undo"
-]
-function CreateButtons(){
-    for (let index = 0; index < bttnArr.length; index++) {
-        const element = <button name={bttnArr[0]}></button>;
-        return element;
-    }
-}
+const navItemList = ["Add Task", "Delete Task", "Undo"];
+const navBttnList : Array<any> = [];
+navItemList.forEach((item, index) => {
+    navBttnList.push(<Button text={navItemList[index]} fontSize={"20px"} backgroundColor={"black"} textColor={"white"} padding={"10px"}></Button>)
+});
 
-const SideNav = (props: Props) => {
+const SideNav = (props: NavProps) => {
     return (
         <div className={styles.container} id={styles.sideNav}>
             <h2>{props.name}</h2>
-            {CreateButtons()}
-            {/* <h1 className={styles.menuButtons}>Add Task</h1>
-            <h1 className={styles.menuButtons}>Delete Task</h1>
-            <h1 className={styles.menuButtons}>Undo</h1> */}
+            {navBttnList}
         </div>
     )
 };
